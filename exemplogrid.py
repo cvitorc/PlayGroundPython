619fbd6736d1a83bd47b401c7e461fe6df32e1c0
Carlos Vitor de Alencar Carvalho <81836696+cvitorc@users.noreply.github.com>
2021-04-02 23:37:15 -0300

    Create exemplogrid.py

---
+++ exemplogrid.py
@@ -0,0 +1,17 @@
+from tkinter import *
+from tkinter import messagebox
+from tkinter.ttk import *
+from tkinter import filedialog
+
+import numpy as np
+import sympy as sp
+import pandas as pd
+import os
+sp.init_printing(use_latex="mathjax")
+
+# trabalhando com Grid
+
+window = Tk()
+window.title(" Trabalhando com Gerenciador de Layout Grid ")
+
+window.mainloop() 
