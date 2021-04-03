2ff9c2ca7725fe29a78a7dce24904927641f6972
Carlos Vitor de Alencar Carvalho <81836696+cvitorc@users.noreply.github.com>
2021-04-02 23:56:57 -0300

    Exemplo layout Grid

--- exemplogrid.py
+++ exemplogrid.py
@@ -14,4 +14,47 @@ sp.init_printing(use_latex="mathjax")
 window = Tk()
 window.title(" Trabalhando com Gerenciador de Layout Grid ")
 
+#dimensoes da janela
+
+largura = 200
+altura = 100
+
+#dimensoes no sistema
+
+largura_screen = window.winfo_screenwidth()
+altura_screen = window.winfo_screenheight()
+
+#posicao da janela
+
+posx = largura_screen*0.5 - largura*0.5
+posy = altura_screen*0.5 - altura*0.5
+
+#definir a geometry
+
+window.geometry("%dx%d+%d+%d" % (largura, altura, posx, posy))
+
+lbl1 = Label (window, text = "Login:")
+
+
+lbl2 = Label (window, text = "Senha:")
+
+
+ed1 = Entry (window)
+ed2 = Entry (window)
+
+bt1 = Button (window, text = "Confirmar")
+
+#definindo gerenciador de layout
+
+lbl1.grid(row=0, column = 0)
+lbl2.grid(row=1, column = 0)
+ed1.grid(row=0, column = 1)
+ed2.grid(row=1, column = 1)
+bt1.grid(row=2, column = 1)
+
+
+
+
+
+
 window.mainloop() 
